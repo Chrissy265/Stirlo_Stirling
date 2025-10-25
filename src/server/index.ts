09a374d315ea61784815562cc8a11b94419ace30
toby148 <48911677-toby148@users.noreply.replit.com>
2025-10-25 22:42:41 +0000

    Restored to '2f19fe48de6a42ce49375343477f76a49edd35bd'
    Replit-Restored-To: 2f19fe48de6a42ce49375343477f76a49edd35bd

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+const PORT = 5001;
 
 app.use(express.json());
 app.use(cookieParser());
